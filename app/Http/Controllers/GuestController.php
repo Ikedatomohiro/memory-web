506188050509e11baae46fbdd31c1dffab5002b2
Tomohiro Ikeda <tomo@TomohironoMacBook-Pro-2.local>
2022-05-08 23:01:52 +0900

    Guest更新とリファクタリング

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -11,6 +11,30 @@ use Validator;
 
 class GuestController extends Controller
 {
+    var $retualsList = [
+        '通夜',
+        '告別式',
+    ];
+
+    var $relationsList = [
+        '故人様',
+        '喪主様',
+        'ご家族',
+        'その他',
+    ];
+
+    var $groupsList = [
+        '会社関係',
+        'お取引先',
+        '学校関係',
+        '官公庁',
+        '各種団体',
+        '町内会',
+        'ご友人',
+        'ご親戚',
+        'その他',
+    ];
+
     /**
      * コンストラクタ
      *
@@ -36,9 +60,9 @@ class GuestController extends Controller
             'relations_other' => '',
             'groups_other'    => '',
             'stored'          => false,
-            'retuals'         => $this->retualsList(),
-            'relations'       => $this->relationsList(),
-            'groups'          => $this->groupsList(),
+            'retuals'         => $this->retualsList,
+            'relations'       => $this->relationsList,
+            'groups'          => $this->groupsList,
         ];
         return $param;
     }
@@ -58,9 +82,6 @@ class GuestController extends Controller
         $this->isEvent($event);
         $param = $this->initParam();
         $param['event'] = $event;
-        $value = $param['guest']->guest_name;
-        // var_dump(htmlspecialchars($value ?? '', ENT_QUOTES, 'UTF-8', true));
-        // var_dump($param['guest']->guest_name);exit();
         return view('guest.create', $param);
     }
 
@@ -73,25 +94,10 @@ class GuestController extends Controller
      */
     public function store(Request $request)
     {
-        // イベント取得し、なければエラー
-        $event = Event::getEvent($request->event_hash);
-        // バリデーション作成
-        $this->isEvent($event);
-        $guest_request = new GuestRequest();
-        $validator = Validator::make($request->all(),
-                                     $guest_request->rules(),
-                                     $guest_request->messages(),
-        );
-        if ($validator->fails()) {
-            return redirect('/events')
-                            ->withErrors($validator)
-                            ->withInput();
-        }
-        $retuals   = !is_null($request->retuals) ? implode(',', $request->retuals) : null;
-        $relations = !is_null($request->relations) ? implode(',', $request->relations) : null;
-        $groups    = !is_null($request->groups) ? implode(',', $request->groups) : null;
+        // 入力チェック
+        $this->inputCheck($request);
         Guest::create([
-            'event_id'        => $event->event_id,
+            'event_id'        => $this->event->event_id,
             'guest_hash'      => \Util::createHash($request->guest_name),
             'guest_name'      => $request->guest_name,
             'company_name'    => $request->company_name,
@@ -99,14 +105,14 @@ class GuestController extends Controller
             'address'         => $request->address,
             'tel'             => $request->tel,
             'description'     => $request->description,
-            'retuals'         => $retuals,
-            'relations'       => $relations,
+            'retuals'         => !is_null($request->retuals) ? implode(',', $request->retuals) : null,
+            'relations'       => !is_null($request->relations) ? implode(',', $request->relations) : null,
             'relations_other' => $request->relations_other,
-            'groups'          => $groups,
+            'groups'          => !is_null($request->groups) ? implode(',', $request->groups) : null,
             'groups_other'    => $request->groups_other,
         ]);
         $param = $this->initParam();
-        $param['event'] = $event;
+        $param['event'] = $this->event;
         $param['stored'] = true;
         return view('guest.create', $param);
     }
@@ -132,20 +138,35 @@ class GuestController extends Controller
         $param = $this->initParam();
         $param['event'] = $event;
         $param['guest'] = $guest;
-        return view('guest.edit', $param);        
+        return view('guest.edit', $param);
     }
 
     /**
      * 来客情報更新
      * 
-     * 
+     * @access public
+     * @param  
      * 
      */
-    public function update($hash)
+    public function update(Request $request, $hash)
     {
-        Guest::where('guest_hash', $hash)->first();
-        
-        echo 's..sjdljka';exit();
+        // 入力チェック
+        $this->inputCheck($request);
+        // 来客情報更新
+        Guest::where('guest_hash', $hash)->update([
+            'guest_name'      => $request->guest_name,
+            'company_name'    => $request->company_name,
+            'zip_code'        => $request->zip_code,
+            'address'         => $request->address,
+            'tel'             => $request->tel,
+            'description'     => $request->description,
+            'retuals'         => !is_null($request->retuals) ? implode(',', $request->retuals) : null,
+            'relations'       => !is_null($request->relations) ? implode(',', $request->relations) : null,
+            'relations_other' => $request->relations_other,
+            'groups'          => !is_null($request->groups) ? implode(',', $request->groups) : null,
+            'groups_other'    => $request->groups_other,
+        ]);
+        return redirect(route('events.show', $this->event->event_hash));
     }
 
     /**
@@ -154,12 +175,22 @@ class GuestController extends Controller
      * @access public
      * 
      */
-    public function inputCheck()
+    public function inputCheck(Request $request)
     {
-        // イベントが存在するか
-
-        // バリデーション
-
+        // イベント取得し、なければエラー
+        $this->event = Event::getEvent($request->event_hash);
+        // バリデーション作成
+        $this->isEvent($this->event);
+        $guest_request = new GuestRequest();
+        $validator = Validator::make($request->all(),
+                                     $guest_request->rules(),
+                                     $guest_request->messages(),
+        );
+        if ($validator->fails()) {
+            return redirect('/events')
+                            ->withErrors($validator)
+                            ->withInput();
+        }
         // ご芳名か会社名のいずれかは入力されているか
 
         // 会社名またはご芳名は必須
@@ -215,60 +246,6 @@ class GuestController extends Controller
         Guest::where('guest_hash', $request->guest)->update(['del_flg' => 1]);
 
 
-    }
-
-    /**
-     * 儀式リスト
-     * 
-     * @access public
-     * @return array $retuals
-     */
-    public function retualsList()
-    {
-        $retuals = [
-            '通夜',
-            '告別式',
-        ];
-        return $retuals;
-    }
-
-    /**
-     * 儀式リスト
-     * 
-     * @access public
-     * @return array $relations
-     */
-    public function relationsList()
-    {
-        $relations = [
-            '故人様',
-            '喪主様',
-            'ご家族',
-            'その他',
-        ];
-        return $relations;
-    }
-
-    /**
-     * 儀式リスト
-     * 
-     * @access public
-     * @return array $groups
-     */
-    public function groupsList()
-    {
-        $groups = [
-            '会社関係',
-            'お取引先',
-            '学校関係',
-            '官公庁',
-            '各種団体',
-            '町内会',
-            'ご友人',
-            'ご親戚',
-            'その他',
-        ];
-        return $groups;
     }
 
 }
--- resources/views/guest/input.blade.php
+++ resources/views/guest/input.blade.php
@@ -4,7 +4,7 @@
             ご参加
             @foreach ($retuals as $key => $retual)
                 @if (!is_null($guest->retuals) && in_array($key, $guest->retuals))
-            <input type="checkbox" name="retuals[{{ $key }}]" value="{{ $key }}" checked>{{ $retual }}
+            <input type="checkbox" name="retuals[{{ $key }}]" value="{{ $key }}" {{ (!is_null($guest->retuals) && in_array($key, $guest->retuals)) ? 'checked' : '' }} />{{ $retual }}
                 @else
             <input type="checkbox" name="retuals[{{ $key }}]" value="{{ $key }}">{{ $retual }}
                 @endif
@@ -33,22 +33,14 @@
         <p>
             どなたのご関係ですか
             @foreach ($relations as $key => $relation)
-                @if (!is_null($guest->relations) && in_array($key, $guest->relations))
-            <input type="checkbox" name="relations[{{ $key }}]" value="{{ $key }}" checked>{{ $relation }}
-                @else
-            <input type="checkbox" name="relations[{{ $key }}]" value="{{ $key }}">{{ $relation }}
-                @endif
+            <input type="checkbox" name="relations[{{ $key }}]" value="{{ $key }}" {{ (!is_null($guest->relations) && in_array($key, $guest->relations)) ? 'checked' : '' }} />{{ $relation }}
             @endforeach
             <input type="text" name="relations_other" value="{{ old('relations_other', $guest->relations_other) }}">
         </p>
         <p>
             どのようなご関係ですか
             @foreach ($groups as $key => $group)
-                @if (!is_null($guest->groups) && in_array($key, $guest->groups))
-            <input type="checkbox" name="groups[{{ $key }}]" value="{{ $key }}" checked>{{ $group }}
-                @else
-            <input type="checkbox" name="groups[{{ $key }}]" value="{{ $key }}">{{ $group }}
-                @endif
+            <input type="checkbox" name="groups[{{ $key }}]" value="{{ $key }}" {{ (!is_null($guest->groups) && in_array($key, $guest->groups)) ? 'checked' : '' }} />{{ $group }}
             @endforeach
             <input type="text" name="groups_other" value="{{ old('groups_other', $guest->groups_other) }}">
         </p>
