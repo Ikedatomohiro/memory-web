6100ffeebc33d0d56ee7c479b09829f34ed5f548
tomohiro <to.tomo.tomohiro@gmail.com>
2022-06-18 15:56:54 +0900

    トップページの編集

--- resources/views/event/edit.blade.php
+++ resources/views/event/edit.blade.php
@@ -11,46 +11,39 @@
 @section('content')
 <div>
     <p>
-    <li class="input-item">
-            <span class="title">
-                イベント名
-            </span>
-            <input type="text" name="event_name" class="input-area" value="{{ old('event_name', $event->event_name) }}" placeholder="例）葬儀" autocomplete="off" form="event-input">&nbsp;
-            <span class="delete"></span>
-        </li>
         <li class="input-item">
             <span class="title">
                 イベント名
             </span>
-            <input type="text" name="event_name" class="input-area" value="{{ old('event_name', $event->event_name) }}" placeholder="例）葬儀" autocomplete="off" form="event-input">&nbsp;
+            <input type="text" name="event_name" class="input-area" value="{{ old('event_name', $event->event_name) }}" autocomplete="off" form="event-input">&nbsp;
             <span class="delete"></span>
         </li>
         <li class="input-item">
             <span class="title">
-                イベント名
+                開催日
             </span>
-            <input type="text" name="event_name" class="input-area" value="{{ old('event_name', $event->event_name) }}" placeholder="例）葬儀" autocomplete="off" form="event-input">&nbsp;
+            <input type="text" name="event_name" class="input-area" value="{{ old('hold_date', $event->hold_date) }}" autocomplete="off" form="event-input">&nbsp;
             <span class="delete"></span>
         </li>
         <li class="input-item">
             <span class="title">
-                イベント名
+                開催場所
             </span>
-            <input type="text" name="event_name" class="input-area" value="{{ old('event_name', $event->event_name) }}" placeholder="例）葬儀" autocomplete="off" form="event-input">&nbsp;
+            <input type="text" name="event_name" class="input-area" value="{{ old('hold_place', $event->hold_place) }}" autocomplete="off" form="event-input">&nbsp;
             <span class="delete"></span>
         </li>
         <li class="input-item">
             <span class="title">
-                イベント名
+                主催者
             </span>
-            <input type="text" name="event_name" class="input-area" value="{{ old('event_name', $event->event_name) }}" placeholder="例）葬儀" autocomplete="off" form="event-input">&nbsp;
+            <input type="text" name="event_name" class="input-area" value="{{ old('organizer_name', $event->organizer_name) }}" autocomplete="off" form="event-input">&nbsp;
             <span class="delete"></span>
         </li>
         <li class="input-item">
             <span class="title">
-                イベント名
+                備考
             </span>
-            <input type="text" name="event_name" class="input-area" value="{{ old('event_name', $event->event_name) }}" placeholder="例）葬儀" autocomplete="off" form="event-input">&nbsp;
+            <textarea type="text" name="event_name" class="input-area" autocomplete="off" form="event-input">{{ old('description', $event->description) }}</textarea>&nbsp;
             <span class="delete"></span>
         </li>
     </p>
